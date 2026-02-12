f945b09c4e9d255350ed5575c75ab3b45ba62c32
Tanveer <tanveeransari5663@gmail.com>
2026-02-13 00:54:22 +0530

    changecookie setting

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -10,8 +10,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: ENV.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: false,
+   
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
