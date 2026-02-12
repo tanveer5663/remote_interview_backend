6fc58660fc472d80055716ebdf3543d850f1727b
Tanveer <tanveeransari5663@gmail.com>
2026-02-13 01:03:39 +0530

    cors fix

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ import cors from "cors";
 const app = express();
 const port = ENV.PORT || 6000;
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // app.use(clerkMiddleware());
 
