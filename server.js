5ca8c9f4b4ebace380e7a00e9b870cc39cec3d67
Tanveer Ansari <tanveeransari5663@gmail.com>
2026-02-13 00:48:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const port = ENV.PORT || 6000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // app.use(clerkMiddleware());
 
